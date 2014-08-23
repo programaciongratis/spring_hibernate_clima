0c6a7f9bc94301ec2e2bc1efa95b61f5eff8cdd4
antonio25x <antonio17x@hotmail.com>
2014-08-23 15:36:48 -0400

    Creando la estructura del proyecto.

---
+++ src/main/java/com/programaciongratis/aplicacion/App.java
@@ -0,0 +1,13 @@
+package com.programaciongratis.aplicacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
